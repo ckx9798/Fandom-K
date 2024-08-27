a779455316a5c6c7c612f673e7beeb6d33e9a930
byeolee1221 <zicsso1221@gmail.com>
2024-08-27 17:13:00 +0900

    [Fix] ListPage 파일 코드 병합 후 커밋 안된 것 추가

--- src/pages/list/ListPage.jsx
+++ src/pages/list/ListPage.jsx
@@ -1,18 +1,12 @@
-import CreditStatus from "../../components/list/CreditStatus";
-
+import CreditStatus from '../../components/list/CreditStatus';
 import Header from '../../components/Header';
 
 const ListPage = () => {
     return (
-        <div>
-            <p>
-                list
-                <Header />
-            </p>
-        </div>
-      <>
-        <CreditStatus />
-      </>
+        <>
+            <Header />
+            <CreditStatus />
+        </>
     );
 };
 
