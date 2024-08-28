b39dd4e177e4514be220053ec1ee1ef0f4967cec
mungyun <mungyun1234@naver.com>
2024-08-28 12:18:09 +0900

    [Chore] MyPage 상태값 전역변수로 변경

--- src/pages/my/components/IdolProfile.jsx
+++ src/pages/my/components/IdolProfile.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import deleteIcon from '../../../assets/icon/Icon-delete.svg';
 import checkIcon from '../../../assets/icon/ic_check.svg';
 
-const IdolProfile = ({ idol, selected = false, onDelete }) => {
+const IdolProfile = ({ idol, selected = false, onDelete, onCheck }) => {
     const [checked, setChecked] = useState(false);
+
+    const handleCheckClick = () => {
+        const newChecked = !checked;
+        setChecked(newChecked);
+        onCheck(idol, newChecked);
+    };
 
     const handleDeleteClick = () => onDelete(idol.id);
 
     return (
-        <IdolCard
-            selected={selected}
-            onClick={() => {
-                setChecked(!checked);
-            }}
-        >
-            <IdolImgContainer selected={selected} checked={checked}>
+        <IdolCard selected={selected} onClick={handleCheckClick}>
+            <IdolImgContainer selected={selected} onClick={handleCheckClick} checked={checked}>
                 <IdolImg src={idol.profilePicture} selected={selected} checked={checked} />
                 {checked && !selected && (
                     <Overlay>
