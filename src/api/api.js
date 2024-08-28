91ec36bab7a7b144a8c787eea661e76afca7fa16
BugO <103034239+ChungO5@users.noreply.github.com>
2024-08-28 11:06:26 +0900

    [Fix]커밋 수정

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 // api instance
 export const instance = axios.create({
-    baseURL: 'https://fandom-k-api.vercel.app/8-3',
+    baseURL: 'https://fandom-k-api.vercel.app/9-4',
     timeout: 3000,
 });
