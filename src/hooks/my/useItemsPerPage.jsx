1fd248202d556c4ae60ad5a4929247b05a323922
mungyun <mungyun1234@naver.com>
2024-08-31 22:02:10 +0900

    [Feat] MyPage 무한 스크롤 기능 구현

--- src/hooks/my/useItemsPerPage.jsx
+++ src/hooks/my/useItemsPerPage.jsx
@@ -1,27 +1,36 @@
 import { useState, useEffect } from 'react';
 
-function useItemsPerPage() {
-    const [itemsPerPage, setItemsPerPage] = useState(16);
+// 화면 크기를 나타내는 상수 객체
+const SIZES = {
+    mobile: 768,
+    tablet: 1280,
+};
+
+// 화면 크기에 따라 보여줄 아이템 개수를 동적으로 설정
+function useItemsPerPage(items = { mobile: 6, tablet: 8, desktop: 16 }) {
+    const [itemsPerPage, setItemsPerPage] = useState(items.desktop);
 
     useEffect(() => {
+        // 화면 크기에 따라 아이템 개수를 업데이트하는 함수
         const updateItemsPerPage = () => {
             const width = window.innerWidth;
-            if (width <= 768) {
-                setItemsPerPage(6);
-            } else if (width <= 1280) {
-                setItemsPerPage(8);
+            if (width <= SIZES.mobile) {
+                setItemsPerPage(items.mobile); // 모바일 화면일 때
+            } else if (width <= SIZES.tablet) {
+                setItemsPerPage(items.tablet); // 테블릿 화면일 때
             } else {
-                setItemsPerPage(16);
+                setItemsPerPage(items.desktop); // 데스크탑 화면일 때
             }
         };
 
         updateItemsPerPage();
         window.addEventListener('resize', updateItemsPerPage);
 
+        // 컴포넌트 언마운트 시 이벤트 리스너 제거
         return () => {
             window.removeEventListener('resize', updateItemsPerPage);
         };
-    }, []);
+    }, [items]);
 
     return itemsPerPage;
 }
